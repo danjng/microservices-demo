61cdb92e6eb0008d43c6c3fdb197d5e3b388e6d7
Andrew Weiss <anweiss@docker.com>
2016-10-12 13:29:18 -0400

    initial commit

---
+++ book/src/main/java/com/brownfield/pss/book/component/BookingException.java
@@ -0,0 +1,10 @@
+package com.brownfield.pss.book.component;
+
+public class BookingException extends RuntimeException  {
+	
+	private static final long serialVersionUID = 5636869584438250866L;
+
+	public BookingException(String message){
+		super(message);
+	}
+}
